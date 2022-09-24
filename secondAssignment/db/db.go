6fd18df191a4828790e20f75aaa58dd79f9b4f58
fiyuang <fitrianggraini96@gmail.com>
2022-09-24 09:27:38 +0700

    create table orders & items

--- secondAssignment/db/db.go
+++ secondAssignment/db/db.go
@@ -3,6 +3,7 @@ package db
 import (
 	"database/sql"
 	"fmt"
+	"log"
 
 	_ "github.com/lib/pq"
 )
@@ -32,5 +33,40 @@ func InitializeDB() {
 	if err != nil {
 		panic(err)
 	}
+
+	createOrdersTable := `
+		CREATE TABLE IF NOT EXISTS orders (
+			order_id SERIAL PRIMARY KEY,
+			customer_name VARCHAR(255) NOT NULL,
+			ordered_at timestamptz DEFAULT now()
+		)
+	`
+	createItemsTable := `
+		CREATE TABLE IF NOT EXISTS items (
+			item_id SERIAL PRIMARY KEY,
+			order_id int NOT NULL,
+			item_code VARCHAR(255) NOT NULL,
+			description VARCHAR(255) NOT NULL,
+			quantity int NOT NULL,
+			ordered_at timestamptz DEFAULT now()
+		)
+	`
+
+	createTable(createOrdersTable)
+	createTable(createItemsTable)
+
 	fmt.Println("Successfully connected to database!")
+}
+
+func createTable(tableName string) {
+	_, err = db.Exec(tableName)
+
+	if err != nil {
+		log.Fatal("Error creating table:", err.Error())
+		return
+	}
+}
+
+func GetDB() *sql.DB {
+	return db
 }
